45fd731391c954b99537734346eba5304407ac52
usamaSaeed93 <kembourne93@gmail.com>
2026-07-31 15:42:04 +0500

    whatsapp number

--- frontend/components/WhatsAppButton.tsx
+++ frontend/components/WhatsAppButton.tsx
@@ -8,7 +8,7 @@ interface WhatsAppButtonProps {
 }
 
 export function WhatsAppButton({
-    phoneNumber = "4401277715069",
+    phoneNumber = "447751798827",
     message = "Hello! I'm interested in your car tuning services."
 }: WhatsAppButtonProps) {
     const [isHovered, setIsHovered] = useState(false);
--- frontend/lib/constants/contact.ts
+++ frontend/lib/constants/contact.ts
@@ -8,5 +8,5 @@ export const CONTACT_PHONE_2_DISPLAY = "+44 7751 798827";
 export const CONTACT_PHONE_2_TEL = "tel:+447751798827";
 export const CONTACT_EMAIL = "info@msperformance.co.uk";
 export const CONTACT_EMAIL_MAILTO = "mailto:info@msperformance.co.uk";
-export const WHATSAPP_NUMBER = "4401277715069";
+export const WHATSAPP_NUMBER = "447751798827";
 export const FILES_PORTAL_URL = "https://files.msperformance.co.uk/panel/login";
